b9cad5441f86f3cfc96309531ad66c44d1e36cf3
Filip Jovanovic <filipj@stud.ntnu.no>
2012-12-13 22:56:45 +0100

    ne

--- src/edu/elfak/chasegame/ChaseGameMapActivity.java
+++ src/edu/elfak/chasegame/ChaseGameMapActivity.java
@@ -11,47 +11,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.google.android.maps.GeoPoint;
-import com.google.android.maps.ItemizedOverlay;
-import com.google.android.maps.MapActivity;
-import com.google.android.maps.MapController;
-import com.google.android.maps.MapView;
-import com.google.android.maps.MyLocationOverlay;
-import com.google.android.maps.Overlay;
-import com.google.android.maps.OverlayItem;
-import com.google.android.maps.Projection;
 
-public class ChaseGameMapActivity extends MapActivity{
 
-	private  MapView mapView;
-	private MyItemizedOverlay itemizedoverlay;
-	private List<Overlay> mapOverlays;
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.map);
-	    mapView = (MapView) findViewById(R.id.mapView);
-	    mapView.setBuiltInZoomControls(true);
-	    
-	    mapOverlays = mapView.getOverlays();
-	    Drawable drawable = this.getResources().getDrawable(R.drawable.androidmarker);
-	    itemizedoverlay = new MyItemizedOverlay(drawable, this);
-	    
-	    addItem();
-	}
+public class ChaseGameMapActivity extends android.support.v4.app.FragmentActivity{
+
 	
-	public void addItem(){
-		GeoPoint point = new GeoPoint(19240000,-99120000);
-		OverlayItem overlayitem = new OverlayItem(point, "Hola, Mundo!", "I'm in Mexico City!");
-		itemizedoverlay.addOverlay(overlayitem);
-		mapOverlays.add(itemizedoverlay);
+	protected void onCreate(Bundle savedInstanceState){
+		 super.onCreate(savedInstanceState);
+		 setContentView(R.layout.map);
 	}
-	
-	@Override
-	protected boolean isRouteDisplayed() {
-	    return false;
-	}
-	
 	// broadcast receiver that handles messages from GCM
 	
 	private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
@@ -61,8 +29,7 @@ public class ChaseGameMapActivity extends MapActivity{
         	Log.v("GCM","Message received"+ message);
         	// Do sth with message
         	Toast.makeText(context, "Received: " + message, Toast.LENGTH_LONG).show();
-        	MapController controller = mapView.getController();
-        	controller.zoomIn();
+
         }
     };
     
@@ -72,10 +39,10 @@ public class ChaseGameMapActivity extends MapActivity{
 		registerReceiver(broadcastReceiver, new IntentFilter(GCMIntentService.TAG));
 	}
  
-	@Override
-	public void onPause() {
+	//@Override
+	/*public void onPause() {
 		super.onPause();
 		unregisterReceiver(broadcastReceiver);
-	}
+	}*/
 
 }
