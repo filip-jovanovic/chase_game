2acb2aff9945669d7b8661e758d3fbffe66dc5b4
filip-jovanovic <filipjovanovic@elfak.rs>
2013-01-13 02:39:17 +0100

    Update src/edu/elfak/chasegame/CreateGameActivity.java

--- src/edu/elfak/chasegame/CreateGameActivity.java
+++ src/edu/elfak/chasegame/CreateGameActivity.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package edu.elfak.chasegame;
 
 import java.util.ArrayList;
@@ -42,47 +41,3 @@ public class CreateGameActivity extends Activity implements OnClickListener {
 	    }
 	}
 }
-=======
-package edu.elfak.chasegame;
-
-import java.util.ArrayList;
-
-import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-public class CreateGameActivity extends Activity implements OnItemClickListener {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.create_game);
-		
-		ArrayList<String> mape = HTTPHelper.getMapList();
-		
-		ListView lista = (ListView) findViewById(R.id.mapList);
-		
-		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
-						(this,android.R.layout.simple_list_item_1, mape);
-		lista.setAdapter(arrayAdapter);
-		
-		lista.setOnItemClickListener(this);
-	}
-
-	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-		String item = ((TextView)arg1).getText().toString();
-        
-        Toast.makeText(getBaseContext(), item, Toast.LENGTH_SHORT).show();
-       
-        Intent i = new Intent(this, MapActivity.class);
-		startActivity(i);		
-	}
-}
->>>>>>> services added
