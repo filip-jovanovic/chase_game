43a97de673aad9eb14d7a5b2b6599f753f0dac77
filip-jovanovic <filipjovanovic@elfak.rs>
2013-02-12 14:58:35 +0100

    server changed

--- src/edu/elfak/chasegame/SnippetAdapter.java
+++ src/edu/elfak/chasegame/SnippetAdapter.java
@@ -1,20 +1,18 @@
 package edu.elfak.chasegame;
 
-
-
 /***
-Copyright (c) 2012 CommonsWare, LLC
-Licensed under the Apache License, Version 2.0 (the "License"); you may not
-use this file except in compliance with the License. You may obtain a copy
-of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
-by applicable law or agreed to in writing, software distributed under the
-License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
-OF ANY KIND, either express or implied. See the License for the specific
-language governing permissions and limitations under the License.
+ Copyright (c) 2012 CommonsWare, LLC
+ Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ use this file except in compliance with the License. You may obtain a copy
+ of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+ by applicable law or agreed to in writing, software distributed under the
+ License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ OF ANY KIND, either express or implied. See the License for the specific
+ language governing permissions and limitations under the License.
 
-From _The Busy Coder's Guide to Android Development_
-  http://commonsware.com/Android
-*/
+ From _The Busy Coder's Guide to Android Development_
+ http://commonsware.com/Android
+ */
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,42 +20,46 @@ import android.widget.TextView;
 import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter;
 import com.google.android.gms.maps.model.Marker;
 
-
 class SnippetAdapter implements InfoWindowAdapter {
-LayoutInflater inflater=null;
+	LayoutInflater inflater = null;
 
-SnippetAdapter(LayoutInflater inflater) {
-  this.inflater=inflater;
-}
+	SnippetAdapter(LayoutInflater inflater) {
+		this.inflater = inflater;
+	}
 
-@Override
-public View getInfoWindow(Marker marker) {
-  return(null);
-}
+	@Override
+	public View getInfoWindow(Marker marker) {
+		return (null);
 
-@Override
-public View getInfoContents(Marker marker) {
-  View popup=inflater.inflate(R.layout.snippetlayout, null);
+	}
 
-  TextView snippetText=(TextView)popup.findViewById(R.id.title);
-  TextView amount=(TextView)popup.findViewById(R.id.bankamount);
-  
-  snippetText.setText(marker.getTitle());
-  snippetText=(TextView)popup.findViewById(R.id.snippet);
- 
-  if(marker.getSnippet()!=null&&(amount.getText().length()==0)){
-	  String s[] = marker.getSnippet().split("\nnovac"); 
-	  snippetText.setText(s[0]);
-	  amount.setText(s[1]);
-  }
-  else{
-	  popup.findViewById(R.id.caption).setVisibility(View.GONE);
-	  popup.findViewById(R.id.bankamount).setVisibility(View.GONE);
-	  popup.findViewById(R.id.amountcaption).setVisibility(View.GONE);
-	 
-	  
-  }
-  
-  return(popup);
-}
+	@Override
+	public View getInfoContents(Marker marker) {
+		View popup = inflater.inflate(R.layout.snippetlayout, null);
+		
+		TextView snippetText = (TextView) popup.findViewById(R.id.title);
+		TextView amount = (TextView) popup.findViewById(R.id.bankamount);
+
+		snippetText.setText(marker.getTitle());
+		snippetText = (TextView) popup.findViewById(R.id.snippet);
+		if (marker.getSnippet() != null) {
+			if (marker.getSnippet().startsWith("novac")) {
+				popup.findViewById(R.id.caption).setVisibility(View.GONE);
+				snippetText.setVisibility(View.GONE);
+				String s[] = marker.getSnippet().split("novac");
+				amount.setText(s[0]);
+			} else if ((amount.getText().length() == 0)) {
+				String s[] = marker.getSnippet().split("\nnovac");
+				snippetText.setText(s[0]);
+				amount.setText(s[1]);
+			}
+		} else {
+			popup.findViewById(R.id.caption).setVisibility(View.GONE);
+			popup.findViewById(R.id.bankamount).setVisibility(View.GONE);
+			popup.findViewById(R.id.amountcaption).setVisibility(View.GONE);
+
+		}
+		
+		return (popup);
+	}
 }
